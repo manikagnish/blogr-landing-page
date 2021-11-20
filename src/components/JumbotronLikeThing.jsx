c1888708a631f521d58d2e8b691356fd2293d989
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-11-20 23:21:18 +0530

    completed jumbotron section

--- src/components/JumbotronLikeThing.jsx
+++ src/components/JumbotronLikeThing.jsx
@@ -1,5 +1,18 @@
-function JumbotronLikeThing() {
-  return <div>JumbotronLikeThing</div>;
-}
+import phones from "../images/illustration-phones.svg";
 
-export default JumbotronLikeThing;
+export default function JumbotronLikeThing() {
+  return (
+    <div className="jumbotron-like-thing">
+      <img src={phones} alt="vector of phones" />
+      <div className="jumbotron-like-thing__text">
+        <h3>State of the Art Infrastructure</h3>
+        <p>
+          With reliability and speed in mind, worldwide data centers provide the
+          backbone for ultra-fast connectivity. This ensures your site will load
+          instantly, no matter where your readers are, keeping your site
+          competitive.
+        </p>
+      </div>
+    </div>
+  );
+}
