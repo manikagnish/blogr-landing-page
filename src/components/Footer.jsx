f6aa90ffe0eb8a26ea823a501e9c4ca34ff33066
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-11-20 21:29:02 +0530

    created footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,62 @@
+import logo from "../images/logo.svg";
+
 function Footer() {
-  return <div className="footer">footer</div>;
+  return (
+    <footer className="footer">
+      <img src={logo} alt="blogr" className="footer__img" />
+      <div className="footer__container">
+        <h4>Product</h4>
+        <ul>
+          <li>
+            <a href="/#">Overview</a>
+          </li>
+          <li>
+            <a href="/#">Pricing</a>
+          </li>
+          <li>
+            <a href="/#">Marketplace</a>
+          </li>
+          <li>
+            <a href="/#">Features</a>
+          </li>
+          <li>
+            <a href="/#">Integrations</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer__container">
+        <h4>Company</h4>
+        <ul>
+          <li>
+            <a href="/#">About</a>
+          </li>
+          <li>
+            <a href="/#">Team</a>
+          </li>
+          <li>
+            <a href="/#">Blog</a>
+          </li>
+          <li>
+            <a href="/#">Careers</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer__container">
+        <h4>Connect</h4>
+        <ul>
+          <li>
+            <a href="/#">Contact</a>
+          </li>
+          <li>
+            <a href="/#">Newsletter</a>
+          </li>
+          <li>
+            <a href="/#">LinkedIn</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
