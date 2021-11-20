08993b3702f0557ffe223bb5f1528f87a5ab19c5
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-11-20 17:41:23 +0530

    added hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Button from "./Button";
 
 function HeroSection() {
   return (
     <div className="hero-section">
       <Navbar />
-      <h1>hero heading</h1>
+
+      <h1 className="hero-section__title">A modern publishing platform</h1>
+      <p className="hero-section__mission-statement">
+        Grow your audience and build your online brand
+      </p>
+
+      <div className="hero-section__btn-container">
+        <Button text="Start for free" type="btn--solid hero-section__btn" />
+        <Button text="Learn More" type="btn--outline" />
+      </div>
     </div>
   );
 }
